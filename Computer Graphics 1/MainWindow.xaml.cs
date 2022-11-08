980f03b6bd2ca25974b760213e1fdbf6ef73040c
AmpF5 <lolc44t@gmail.com>
2022-11-08 14:28:54 +0100

    blank version

--- Computer Graphics 1/MainWindow.xaml.cs
+++ Computer Graphics 1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Pen = System.Drawing.Pen;
+using Point = System.Drawing.Point;
 
 namespace Computer_Graphics_1
 {
@@ -20,9 +23,52 @@ namespace Computer_Graphics_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(System.Drawing.Color.Black, 1);
+        int index;
         public MainWindow()
         {
             InitializeComponent();
+
+            bm = new Bitmap(1280, 720);
+            g = Graphics.FromImage(bm);
+            g.Clear(System.Drawing.Color.White);
+            
         }
+
+        private void Line_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Rectangle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Circle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private Rectangle GetRect()
+        //{
+        //    Point pointXY;
+        //    Point pointX1Y1;
+        //    Rectangle rect = new();
+        //    rect.RadiusX = Math.Min(pointXY.X, pointX1Y1.X);
+        //    rect.RadiusY = Math.Min(pointXY.Y, pointX1Y1.Y);
+        //    rect.Width = Math.Abs(pointXY.X - pointX1Y1.X);
+        //    rect.Height = Math.Abs(pointXY.Y - pointX1Y1.Y);
+        //    return rect;
+        //}
     }
 }
