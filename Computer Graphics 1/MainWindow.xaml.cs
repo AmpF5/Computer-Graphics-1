fbcfa0c627f04fdc52986d52067281845acbc97b
AmpF5 <lolc44t@gmail.com>
2022-11-08 20:33:00 +0100

    -line working - rectangle wrong coordinates

--- Computer Graphics 1/MainWindow.xaml.cs
+++ Computer Graphics 1/MainWindow.xaml.cs
@@ -6,15 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Pen = System.Drawing.Pen;
-using Point = System.Drawing.Point;
+using Brushes = System.Windows.Media.Brushes;
+using Point = System.Windows.Point;
+using Rectangle = System.Windows.Shapes.Rectangle;
+//using Rectangle = System.Windows.Shapes.Rectangle;
 
 namespace Computer_Graphics_1
 {
@@ -23,52 +27,110 @@ namespace Computer_Graphics_1
     /// </summary>
     public partial class MainWindow : Window
     {
-        Bitmap bm;
-        Graphics g;
-        bool paint = false;
-        Point px, py;
-        Pen p = new Pen(System.Drawing.Color.Black, 1);
         int index;
+        bool IsFirstPoint = true;
+        Point StartPoint;
         public MainWindow()
         {
             InitializeComponent();
-
-            bm = new Bitmap(1280, 720);
-            g = Graphics.FromImage(bm);
-            g.Clear(System.Drawing.Color.White);
-            
         }
 
         private void Line_Click(object sender, RoutedEventArgs e)
         {
-            
+            index = 0;
+
         }
 
         private void Rectangle_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
+            index = 1;
         }
 
         private void Circle_Click(object sender, RoutedEventArgs e)
         {
+            index = 2;
+        }
+
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (IsFirstPoint)
+            {
+                StartPoint = (Mouse.GetPosition(this));
+                IsFirstPoint = false;
+            }
+            else
+            {
+                switch (index)
+                {
+                    case 0:
+                        Line line = new Line()
+                        {
+                            X1 = StartPoint.X,
+                            Y1 = StartPoint.Y,
+                            X2 = Mouse.GetPosition(this).X,
+                            Y2 = Mouse.GetPosition(this).Y,
+                            Stroke = Brushes.Black 
+                        };
+                        Canvas_Board.Children.Add(line);
+                        break;
+                    case 1:
+                        double X1 = StartPoint.X;
+                        double Y1 = StartPoint.Y;
+                        double X2 = Mouse.GetPosition(this).X;
+                        double Y2 = Mouse.GetPosition(this).Y;
+                        Rectangle rectangle = new Rectangle()
+                        {
+                            Height = Math.Sqrt(Math.Pow(X1 - X1, 2) + Math.Pow(Y2 - Y1, 2)),
+                            Width = Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y1, 2)),
+                            Stroke = Brushes.Black,
+                            StrokeThickness = 2,
+                        };
+                        //rectangle.
+                        Canvas.SetTop(rectangle, X1);
+                        Canvas.SetLeft(rectangle, Y1);
+                        Canvas_Board.Children.Add(rectangle);
+                        break;
+                }
+                IsFirstPoint = true;
+            }
 
         }
 
-        //private Rectangle GetRect()
-        //{
-        //    Point pointXY;
-        //    Point pointX1Y1;
-        //    Rectangle rect = new();
-        //    rect.RadiusX = Math.Min(pointXY.X, pointX1Y1.X);
-        //    rect.RadiusY = Math.Min(pointXY.Y, pointX1Y1.Y);
-        //    rect.Width = Math.Abs(pointXY.X - pointX1Y1.X);
-        //    rect.Height = Math.Abs(pointXY.Y - pointX1Y1.Y);
-        //    return rect;
-        //}
+        private void Canvas_Board_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if (!IsFirstPoint)
+            //{
+            //    if (Canvas_Board.Children.Count > 0)
+            //    {
+            //        var child = (from c in Canvas_Board.Children.OfType<FrameworkElement>()
+            //                     where "tempLine".Equals(c.Tag)
+            //                     select c).First();
+            //        if (child != null)
+            //        {
+            //            Canvas_Board.Children.Remove(child);
+            //        }
+            //    }
+            //    switch (index)
+            //    {
+            //        case 0:
+            //            Line line = new Line()
+            //            {
+            //                Tag = "tempLine",
+            //                X1 = StartPoint.X,
+            //                Y1 = StartPoint.Y,
+            //                X2 = Mouse.GetPosition(this).X,
+            //                Y2 = Mouse.GetPosition(this).Y,
+            //                Stroke = Brushes.Black
+            //            };
+            //            Canvas_Board.Children.Add(line);
+            //            return;
+            //    }
+            //}
+        }
+
+        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
